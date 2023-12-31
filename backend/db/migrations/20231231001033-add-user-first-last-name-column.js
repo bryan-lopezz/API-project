957dfea0cdbad04ee58593e4590e2248c7e9b03c
Bryan <blopez449@gmail.com>
2023-12-30 17:59:04 -0800

    Add options object to migration and seeders queryInterface

--- backend/db/migrations/20231231001033-add-user-first-last-name-column.js
+++ backend/db/migrations/20231231001033-add-user-first-last-name-column.js
@@ -1,21 +1,26 @@
 'use strict';
 
 /** @type {import('sequelize-cli').Migration} */
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+ options.schema = process.env.SCHEMA; // define your schema in options object
+};
+options.tableName = 'Users';
+
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.addColumn('Users', 'firstName', {
+    await queryInterface.addColumn(options, 'firstName', {
       type: Sequelize.STRING,
-      allowNull: true,
   });
 
-    await queryInterface.addColumn('Users', 'lastName', {
+    await queryInterface.addColumn(options, 'lastName', {
       type: Sequelize.STRING,
-      allowNull: true,
   });
   },
 
   async down (queryInterface, Sequelize) {
-    await queryInterface.removeColumn('Users', 'firstName');
-    await queryInterface.removeColumn('Users', 'lastName');
+    await queryInterface.removeColumn(options, 'firstName');
+    await queryInterface.removeColumn(options, 'lastName');
   },
 };
