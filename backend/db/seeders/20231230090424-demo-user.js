e4c43a04a760bf48854cb30333a49fac3daca82e
Bryan <blopez449@gmail.com>
2023-12-30 19:33:37 -0800

    Update seeder to reflect changes to User attributes

--- backend/db/seeders/20231230090424-demo-user.js
+++ backend/db/seeders/20231230090424-demo-user.js
@@ -14,17 +14,23 @@ module.exports = {
       {
         email: 'demo@user.io',
         username: 'Demo-lition',
-        hashedPassword: bcrypt.hashSync('password')
+        hashedPassword: bcrypt.hashSync('password'),
+        firstName: 'demo1',
+        lastName: 'demo1',
       },
       {
         email: 'user1@user.io',
         username: 'FakeUser1',
-        hashedPassword: bcrypt.hashSync('password2')
+        hashedPassword: bcrypt.hashSync('password2'),
+        firstName: 'demo2',
+        lastName: 'demo2',
       },
       {
         email: 'user2@user.io',
         username: 'FakeUser2',
-        hashedPassword: bcrypt.hashSync('password3')
+        hashedPassword: bcrypt.hashSync('password3'),
+        firstName: 'demo3',
+        lastName: 'demo3',
       }
     ], { validate: true });
   },
