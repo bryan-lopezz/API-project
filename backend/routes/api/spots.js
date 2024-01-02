95d6401997bd23778341a6360ab2928c63db341c
Bryan <blopez449@gmail.com>
2024-01-02 15:43:39 -0800

    Start spots feature

---
+++ backend/routes/api/spots.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const bcrypt = require('bcryptjs');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { User, Spot, SpotImage, Review } = require('../../db/models');
+
+const router = express.Router();
+
+const validateSignup = [
+  check('email')
+    .exists({ checkFalsy: true })
+    .isEmail()
+    .withMessage('Please provide a valid email.'),
+  check('username')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 4 })
+    .withMessage('Please provide a username with at least 4 characters.'),
+  check('username')
+    .not()
+    .isEmail()
+    .withMessage('Username cannot be an email.'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 6 })
+    .withMessage('Password must be 6 characters or more.'),
+  handleValidationErrors
+];
+
+router.get('/spots', async (req, res) => {
+  const spots = await Spot.findAll({
+    include: [
+      {
+        model: SpotImage
+      },
+      {
+        model: Review
+      }
+    ]
+  })
+  res.status(200).json(spots);
+});
+
+module.exports = router;
