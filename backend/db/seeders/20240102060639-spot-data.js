bd69b85135e8cc2f6c516000928efa14f7aff540
Bryan <blopez449@gmail.com>
2024-01-01 23:43:53 -0800

    Create Spot model, migration, and seeds and association to User

---
+++ backend/db/seeders/20240102060639-spot-data.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const { Spot } = require('../models');
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+};
+/** @type {import('sequelize-cli').Migration} */
+options.tableName = 'Spots';
+
+const spots = [
+  {
+    ownerId: 1,
+    "address": "123 Disney Lane",
+    "city": "San Francisco",
+    "state": "California",
+    "country": "United States of America",
+    "lat": 37.7645358,
+    "lng": -122.4730327,
+    "name": "App Academy",
+    "description": "Place where web developers are created",
+    "price": 123,
+  }
+]
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await Spot.bulkCreate(spots, { validate: true });
+  },
+
+  async down (queryInterface, Sequelize) {
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      address: {[Op.in]: ['123 Disney Lane']}
+    })
+  }
+};
