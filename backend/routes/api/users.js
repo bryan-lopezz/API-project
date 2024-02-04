34f076a8e65fd3c73bafb699abad7b044301c7cc
Bryan <blopez449@gmail.com>
2024-02-03 22:28:49 -0800

    removed password key from sign up error response

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -20,10 +20,6 @@ const validateSignup = [
     .not()
     .isEmail()
     .withMessage('Username cannot be an email.'),
-  check('password')
-    .exists({ checkFalsy: true })
-    .isLength({ min: 6 })
-    .withMessage('Password must be 6 characters or more.'),
   handleValidationErrors
 ];
 
