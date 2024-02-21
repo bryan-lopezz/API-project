f90106d5b12db828710c0873aca96debf43a9392
Bryan <blopez449@gmail.com>
2024-02-20 19:19:49 -0800

    added conditional rendering to spot details page components

--- frontend/src/components/SpotDetailsPage/SpotDetailsPage.jsx
+++ frontend/src/components/SpotDetailsPage/SpotDetailsPage.jsx
@@ -11,7 +11,11 @@ const SpotDetailsPage = () => {
 
   useEffect(() => {
     dispatch(getSpotDetails(spotId))
-  }, [dispatch])
+  }, [dispatch]);
+
+  const handleReserveClick = () => {
+    alert("Feature coming soon!")
+  };
 
   return (
     <>
@@ -19,14 +23,34 @@ const SpotDetailsPage = () => {
       <div>
         {spot && (
           <>
-           <h2>{spot.name}</h2>
-           <h3>{`${spot.city}, ${spot.state}, ${spot.country}`}</h3>
-           <div>
-             {spot.SpotImages.map(image => (
-               <h3 key={image.id}>{image.url}</h3>
-               ))}
-           </div>
-           <img src="https://res.cloudinary.com/lopez-projects/image/upload/v1708474380/stnsomcuuy3msytvwzyi.jpg" alt={`image of ${spot.name}`} />
+            <div className="spot-details-image-container">
+             <h2>{spot.name}</h2>
+             <h3>{`${spot.city}, ${spot.state}, ${spot.country}`}</h3>
+             <img src="https://res.cloudinary.com/lopez-projects/image/upload/v1708474380/stnsomcuuy3msytvwzyi.jpg" alt={`image of ${spot.name}`} />
+             <div>
+               {spot.SpotImages?.map(image => (
+                 <h3 key={image.id}>{image.url}</h3>
+                 ))}
+             </div>
+            </div>
+            <div className="spot-details-info-container">
+              <div className="spot-description-container">
+                <h3>{`Hosted by ${spot.Owner?.firstName}, ${spot.Owner?.lastName}`}</h3>
+                <p>{spot.description}</p>
+              </div>
+              <div className="spot-callout-container">
+                <div className="spot-callout-info">
+                  <span className="spot-callout-price">{`$${spot.price}`}</span>
+                  <span className="night">night</span>
+                  <button
+                    className="spot-callout-button"
+                    onClick={handleReserveClick}
+                    >
+                      Reserve
+                  </button>
+                </div>
+              </div>
+            </div>
           </>
         )}
       </div>
