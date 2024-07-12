af0b10fb800a4d1a929fb83f1c5e1470e1a9b569
Bryan <blopez449@gmail.com>
2024-07-12 10:26:28 -0700

    Add spotImage seed data

--- backend/db/seeders/20240102081753-spotImage-data.js
+++ backend/db/seeders/20240102081753-spotImage-data.js
@@ -281,13 +281,11 @@ module.exports = {
 
   async down(queryInterface, Sequelize) {
     const Op = Sequelize.Op;
+    const spotImageIds = spotImages.map((spotImage) => spotImage.id)
+    console.log("🚀 ~ down ~ spotImageIds:", spotImageIds)
     await queryInterface.bulkDelete(options, {
       id: {
-        [Op.in]: [
-          1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-          21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37,
-          39, 40,
-        ],
+        [Op.in]: spotImageIds,
       },
     });
   },
